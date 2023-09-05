53dc0878303e89cfb286d17ee4a5a3614d984bf6
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-09-05 15:27:19 +0300

    put data into firebase

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,63 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const { title, price, description, id } = props;
+
+  const addToCartHandler = () => {
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+
+
+
+
+    
+
+    // !!! НЕ МОЖНА НАПРЯМУ ЗМІНЮВАТИ ДАННІ STATE в підході нижче! і цей варіант є досить поганим рішенням
+    // const newTotalQuantity = cart.totalQuantity + 1;
+
+    // const updatedItems = cart.items.slice(); // create copy via slice to avoid mutation
+    // const existingItem = updatedItems.find((item) => item.id === id);
+
+    // if (existingItem) {
+    //   const updatedItem = { ...existingItem }; //new obj + copy existing properties
+    //   updatedItem.quantity++;
+    //   updatedItem.totalPrice = updatedItem.totalPrice + price;
+    //   const existingItemIndex = updatedItems.findIndex(
+    //     (item) => item.id === id
+    //   );
+    //   updatedItems[existingItemIndex] = updatedItem;
+    // } else {
+    //   updatedItems.push({
+    //     id: id,
+    //     price: price,
+    //     quantity: 1,
+    //     totalPrice: price,
+    //     name: title,
+    //   });
+    // }
+
+    // const newCart = {
+    //   totalQuantity: newTotalQuantity,
+    //   items: updatedItems,
+    // };
+
+    // dispatch(cartActions.replaceCart(newCart));
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+  };
 
   return (
     <li className={classes.item}>
@@ -13,7 +68,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,18 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
+const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
     totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({     //така маніпуляція можлива завдяки використанню Redux toolkit
-          itemId: newItem.id,  // інакше це вважалося б пряма зміна state - що заборонено без Redux toolkit!
+          id: newItem.id,  // інакше це вважалося б пряма зміна state - що заборонено без Redux toolkit!
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -25,4 +30,18 @@ createSlice({
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
-      const existin
+      const existingItem = state.items.find(item => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
